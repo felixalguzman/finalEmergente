8f7879dddacc301a11e6065a6e813f8635f4abc2
Felix Alejandro Guzman <felixalejandro@outlook.com>
2019-07-27 20:13:24 -0400

    Terminando query's

--- spark.py
+++ spark.py
@@ -1,12 +1,9 @@
 from __future__ import print_function
 
-import sys
-from random import random
-from operator import add
-
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, Row
-from pyspark.sql.types import DoubleType, IntegerType, StringType
+from pyspark.sql.functions import col
+from pyspark.sql.types import Row
+from pyspark.sql import functions as F
 
 if __name__ == "__main__":
     """
@@ -16,37 +13,32 @@ if __name__ == "__main__":
         .builder \
         .appName("PythonPi") \
         .getOrCreate()
-    schema = StructType([
-        StructField("Ciudad", StringType()),
-        StructField("Mes", StringType()),
-        StructField("MontoVenta", IntegerType())
-    ])
 
     sc = spark.sparkContext
     textFile = sc.textFile("hdfs://localhost:9000/dataset.txt")
     partes = textFile.map(lambda l: l.split("\t"))
-    data = partes.map(lambda p: Row(ciudad=p[0],mes=p[1],monto=int(p[2])))
-    # data = partes.map(lambda p: Row(ciudad=p[0], mes=p[1], monto=int(p[2].strip())))
+    data = partes.map(lambda p: Row(ciudad=p[0], mes=p[1], monto=int(p[2])))
     df = spark.createDataFrame(data)
 
     df.printSchema()
-    df.show()
-    # ds = spark.createDataFrame(df, schema=schema)
-    # ds.printSchema()
-    # for line in df:
-    #     print(line)
+    # df.show()
 
-    # df.filter(df['MontoVenta'] > 5000).show()
-    # df.groupBy(df['Ciudad']).sum('MontoVenta').show()
-# partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-# n = 100000 * partitions
-#
-# def f(_):
-#     x = random() * 2 - 1
-#     y = random() * 2 - 1
-#     return 1 if x ** 2 + y ** 2 <= 1 else 0
-#
-# count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-# print("Pi is roughly %f" % (4.0 * count / n))
+    print('total de ventas x ciudad')
+    df.groupBy('ciudad').sum('monto').show()
 
-spark.stop()
+    print('total de ventas x mes')
+    df.groupBy('mes').sum('monto').sort('mes').show()
+
+    print('mes con mayor venta')
+    # df.orderBy(F.desc('monto')).limit(1).show()
+    df.groupBy('mes').sum('monto').orderBy(col('sum(monto)').desc()).limit(1).show()
+
+    print('ciudad con mayor venta')
+    df.groupBy('ciudad').sum('monto').orderBy(col('sum(monto)').desc()).limit(1).show()
+
+    print('Mostrar el Total Ventas para los Meses Enero y Marzo en la Ciudades de Santiago, La Vega y Moca.')
+    df.filter(
+        col('ciudad').eqNullSafe('Santiago de los Caballeros').__or__(col('ciudad').eqNullSafe('Moca')).__or__(col('ciudad').eqNullSafe('La vega')).__and__(col('mes').eqNullSafe('Marzo').__or__(col('mes').eqNullSafe('Enero')))
+    ).groupBy('ciudad', 'mes').sum('monto').show()
+
+    spark.stop()
